96881ce7f4c78c006aae49faf9ae8bde3d4337f0
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-22 12:26:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -177,17 +177,18 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
 )
-     app.add_handler(
 
+app.add_handler(
     MessageHandler(
-
-        filters.PHOTO | filters.VOICE | filters.VIDEO | filters.DOCUMENT | filters.STICKER,
-
+        filters.PHOTO |
+        filters.VOICE |
+        filters.VIDEO |
+        filters.DOCUMENT |
+        filters.STICKER,
         handle_media
-
     )
-
 )
+
 print("🔥 Bot is running...")
 
 app.run_polling()

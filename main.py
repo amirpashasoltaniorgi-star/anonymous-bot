5827ecea1dd1472487429f6931f08582e3ee90f8
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-24 01:35:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -146,13 +146,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     return
      
-if text == "❓ راهنما":
-    await update.message.reply_text(
-        "💬 شروع چت\n🔄 نفر بعدی\n🔚 پایان چت"
-    )
-    return
+    if text == "💬 شروع چت ناشناس":
+        if user_id in pairs:
+            await update.message.reply_text("شما داخل چت هستید")
+        else:
+            await find_partner(user_id, context)
+        return
 
-if text == "🚨 گزارش کاربر":
 
         if user_id not in pairs:
             await update.message.reply_text("❌ الان داخل چت نیستی")

c986c387985f3ae64f36a75227dbfea08428a796
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-24 12:24:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -305,21 +305,23 @@ async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     WHERE count >= 1
     """)
 
-    reported_users = cursor.fetchone()[0]
-cursor.execute("""
-SELECT COALESCE(SUM(count), 0)
-FROM reports
-""")
+        reported_users = cursor.fetchone()[0]
 
-total_reports = cursor.fetchone()[0]
+    cursor.execute("""
+    SELECT COALESCE(SUM(count), 0)
+    FROM reports
+    """)
+
+    total_reports = cursor.fetchone()[0]
+
     await update.message.reply_text(
-    f"📊 پنل ادمین\n\n"
-    f"👥 کاربران: {total_users}\n"
-    f"💬 چت فعال: {active_chats}\n"
-    f"⏳ در انتظار: {waiting}\n"
-    f"🚨 کاربران گزارش‌شده: {reported_users}\n"
-    f"📢 کل گزارش‌ها: {total_reports}"
-)
+        f"📊 پنل ادمین\n\n"
+        f"👥 کاربران: {total_users}\n"
+        f"💬 چت فعال: {active_chats}\n"
+        f"⏳ در انتظار: {waiting}\n"
+        f"🚨 کاربران گزارش‌شده: {reported_users}\n"
+        f"📢 کل گزارش‌ها: {total_reports}"
+    )
         
 async def reports(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id != ADMIN_ID:

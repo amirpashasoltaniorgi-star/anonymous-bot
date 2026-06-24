877f855803a9011e1a73e9023c5721ba598c5e88
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-24 17:30:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ CREATE TABLE IF NOT EXISTS reports (
 """)
 
 conn.commit()
+
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS profiles (
     user_id INTEGER PRIMARY KEY,
     name TEXT,
@@ -30,6 +32,8 @@ CREATE TABLE IF NOT EXISTS profiles (
     bio TEXT
 )
 """)
+
+conn.commit()
 
 conn.commit()
 
@@ -293,20 +297,20 @@ async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     waiting = 1 if waiting_user else 0
 
     cursor.execute("""
-    SELECT COUNT(*)
-    FROM reports
-    WHERE count >= 1
-    """)
+SELECT COUNT(*)
+FROM reports
+WHERE count >= 1
+""")
 
-    
+reported_users = cursor.fetchone()[0]
 
-    await update.message.reply_text(
-        f"📊 پنل ادمین\n\n"
-        f"👥 کاربران: {total_users}\n"
-        f"💬 چت فعال: {active_chats}\n"
-        f"⏳ در انتظار: {waiting}\n"
-        f"🚨 کاربران گزارش‌شده: {reported_users}"
-    )
+await update.message.reply_text(
+    f"📊 پنل ادمین\n\n"
+    f"👥 کاربران: {total_users}\n"
+    f"💬 چت فعال: {active_chats}\n"
+    f"⏳ در انتظار: {waiting}\n"
+    f"🚨 کاربران گزارش‌شده: {reported_users}"
+)
         
 async def reports(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id != ADMIN_ID:

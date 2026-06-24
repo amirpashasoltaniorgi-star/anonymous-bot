14355a0aa790d1cb8c2c44808dfa8bd0eb48531d
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-24 14:04:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         return
         if text == "👦 پسر":
-        cursor.execute(
+    cursor.execute(...)
             "INSERT OR REPLACE INTO genders(user_id, gender) VALUES(?, ?)",
             (user_id, "boy")
         )

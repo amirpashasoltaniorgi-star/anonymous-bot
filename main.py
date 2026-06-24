d6a9667cff9919c01e3b2c3f6046167c11763719
amirpashasoltaniorgi-star <amirpashasoltaniorgi@gmail.com>
2026-06-24 17:45:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ pairs = {}
 
 conn = sqlite3.connect("users.db", check_same_thread=False)
 cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY
+)
+""")
 
+conn.commit()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS reports (
